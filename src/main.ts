fb17d49570dc8afa16bf2f99a07dc5188956515f
Sebastian Hines <sebastian@zebweb.dk>
2024-05-18 09:00:27 +0200

    move

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import WidgetRouter from "./classes/router";
 import mainPage from "./pages/main.page";
 import openProjectPage from "./pages/open-project.page";
 import projectPage from "./pages/project.page";
+import jpeg from 'jpeg-js';
+import fs from 'node:fs';
+import path from 'node:path';
+import { MK3GraphicsController, Mk3Display } from "./classes/MK3Controller";
 
 declare const global: { win: QMainWindow, router: typeof WidgetRouter} //, se: SoundEngine}; //, prisma: PrismaClient };
 global.router = WidgetRouter;
@@ -22,14 +26,21 @@ class Main {
 
   public router = WidgetRouter;
 
-  init() {
+  async init() {
     console.log('Starting MaschinePI');
+
     this._win = new QMainWindow();
     this._win.setWindowTitle("MaschinePI");  
     this._win.setFixedSize(800, 480);    
 
     const controller = new MaschineMk3(createNodeHidAdapter, createNodeUsbAdapter);  
-    //controller.setLCDString("Hello from MaschinePI");
+    await controller.init();    
+
+    const welcomeSplash = jpeg.decode(fs.readFileSync(path.join(process.cwd(), 'assets', 'images', 'maschinepi-splash.jpg')), { useTArray: true, formatAsRGBA: false});    
+    const mk3Gfx = new MK3GraphicsController(controller);
+    mk3Gfx.sendImage(welcomeSplash.data, Mk3Display.left);    
+    mk3Gfx.showVersion();
+    mk3Gfx.padIntro();
     
     this.router.addRoute('main', mainPage);
     this.router.addRoute('open-project', openProjectPage);
